c2e0caf0b0bf21768396962090bfb1c4eca4f8db
Yash Chowdhury <yashchowdhurysn@gmail.com>
2022-11-22 16:53:52 +0530

    okay

---
+++ src/jUnitTestPackage/jUnitFunctions.java
@@ -0,0 +1,16 @@
+package jUnitTestPackage;
+
+public class jUnitFunctions {
+	
+	int addNumbers(int a,int b) {
+		
+		return a+b;
+	}
+	
+	String addStrings(String a,String b) {
+		
+		return a+b;
+	}
+	
+
+}
---
+++ src/jUnitTestPackage/testAddNumbers.java
@@ -0,0 +1,19 @@
+package jUnitTestPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testAddNumbers {
+
+	@Test
+	public void test() {
+		jUnitFunctions junit = new jUnitFunctions();
+		int result = junit.addNumbers(100,200);
+		assertEquals(300,result);
+		
+		
+		//fail("Not yet implemented");
+	}
+
+}
